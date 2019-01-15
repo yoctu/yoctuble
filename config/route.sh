8963963919feed6032e42b6102e557761430276c
dzove855 <v.dzogovic@flash-global.net>
2019-01-15 12:45:01 +0100

    change a lot

--- config/route.sh
+++ config/route.sh
@@ -1,10 +1,4 @@
-ROUTE['/login':'GET']='html::print::out ${html_dir}/login.html'
-ROUTE['/login':'POST']='html::print::out ${html_dir}/login.html'
-ROUTE['/css/.*':'GET']='css::print::out ${uri#*/}'
-ROUTE['/img/.*':'GET']='img::print::out ${uri#*/}'
-ROUTE['/js/.*':'GET']='js::print::out ${uri#*/}'
-ROUTE['/fonts/.*':'GET']='fonts::print::out ${uri#*/}'
-ROUTE['/':'GET']="html::print::out ${html_dir}/home.html"
+ROUTE['/':'GET']="Html::print::out home.html"
 ROUTE['/ajaxlogout':'GET']="session::destroy"
 
 AUTH['/':'GET']="htpasswd"
