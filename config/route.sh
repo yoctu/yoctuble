0283d5928c2be425586860e485a03d9dc0c88623
dzove855 <v.dzogovic@flash-global.net>
2019-01-15 11:31:57 +0100

    Add saml conf

--- config/route.sh
+++ config/route.sh
@@ -17,3 +17,16 @@ AUTH['/fonts/.*':'GET']="none"
 
 LOGIN['/':'GET']="auth::custom::request"
 LOGIN['/':'POST']="auth::custom::request"
+
+# For Saml Auth
+# Route to set for activation saml
+#ROUTE['/acs':'POST']='Saml::retrieve::Identity'
+#ROUTE['/acs':'GET']='Saml::retrieve::Identity'
+#ROUTE['/login':'GET']='Saml::buildAuthnRequest'
+#ROUTE['/':'GET']="Html::print::out ${html_dir}/home.html"
+
+#AUTH['/':'GET']="connect"
+#AUTH['/acs':'POST']="none"
+#AUTH['/acs':'GET']="none"
+
+#LOGIN['/':'GET']="Auth::saml::request"
---
+++ config/saml.sh
@@ -0,0 +1,6 @@
+declare -A SAML
+
+#SAML['spurl']="HOST"
+#SAML['xmltemplate']="${DOCUMENT_ROOT%/}../config/saml/template/auth.xml"
+#SAML['idpxml']="idp file"
+#SAML['privkey']="private_key"
