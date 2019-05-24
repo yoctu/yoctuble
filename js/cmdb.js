ffe14ac8ba3fb5638c7b51eeb758b421a5b59379
lvenier <l.venier@flash-global.net>
2019-05-24 20:21:04 +0200

    add cmdb step2

--- js/cmdb.js
+++ js/cmdb.js
@@ -1,3 +1,57 @@
+function ReallyAddIt() {
+    try {
+        JSON.parse($("#instanceJson").val());
+    } catch (e) {
+        $('#CMDBAddModal').modal('hide');
+        $('#modal-comment').html("not a JSON Useless DEV !!!!");
+        $('#CommentModal').modal('show');
+        return;
+    }
+    var project = $("#select-cmdb-project").val();
+    var env = $("#select-cmdb-env").val();
+    var app = $("#select-cmdb-app").val();
+    var instance = $("#instanceName").val();
+    $.ajax({
+        type: "POST",
+        data: JSON.stringify({ instance: instance }),
+        contentType: "application/json",
+        url: "https://cmdb.dev.yoctu.com/v1/project/"+project+"/environment/"+env+"/application/"+app+"/instances",
+        dataType: "json",
+        success: function (result) {
+            $.ajax({
+                type: "POST",
+                contentType: "application/json",
+                data: $("#instanceJson").val(),
+                url: "https://cmdb.dev.yoctu.com/v1/app/project/"+project+"/environment/"+env+"/application/"+app+"/instance/"+instance+"/section/specific",
+                dataType: "json",
+                success: function (data) {
+                    $('#CMDBAddModal').modal('hide');
+                    cmdb();
+                }
+            });
+        }
+    });
+}
+
+function deleteSpecific(instance) {
+    var project = $("#select-cmdb-project").val();
+    var env = $("#select-cmdb-env").val();
+    var app = $("#select-cmdb-app").val();
+    $.ajax({
+        type: "DELETE",
+        contentType: "application/json",
+        url: "https://cmdb.dev.yoctu.com/v1/project/"+project+"/environment/"+env+"/application/"+app+"/instance/"+instance,
+        dataType: "json",
+        success: function (result) {
+            cmdb();
+        }
+    });
+}
+
+function addIt() {
+    $("#CMDBAddModal").modal('show');
+}
+
 function showSpecific(instance) {
     var project = $("#select-cmdb-project").val();
     var env = $("#select-cmdb-env").val();
@@ -24,10 +78,13 @@ function cmdb() {
         url: "https://cmdb.dev.yoctu.com/v1/project/"+project+"/environment/"+env+"/application/"+app+"/instances",
         dataType: "json",
         success: function (data) {
-            let result = '<table id="cmdb" class="table responsive"><th>Instance Name</th><th>Action</th>';
+            let result = '<div align="right"><button class="btn btn-default btn-danger" onclick="addIt();"><span class="glyphicon glyphicon-plus"></span></button></div><br>';
+            result += '<table id="cmdb" class="table responsive"><th>Instance Name</th><th>Action</th>';
             for (let instance in data) {
-                result += '</tr><td>' + data[instance] + '</td><td><a onclick="showSpecific(\''+data[instance]+'\');"><span class="glyphicon glyphicon-eye-open"></span></td></tr>';
-                
+                result += '</tr><td>' + data[instance] + '</td><td>';
+                result += '<a onclick="showSpecific(\''+data[instance]+'\');"> <span class="glyphicon glyphicon-eye-open"></span> ';
+                result += '<a onclick="deleteSpecific(\''+data[instance]+'\');"> <span class="glyphicon glyphicon-remove"></span> ';
+                result += '</td></tr>';
             }
             result += '</table>';
             $('#inventory-table').html(result);
